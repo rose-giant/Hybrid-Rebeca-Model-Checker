b5aa2e4347b6a0b8a6fdd163b01a48ad02175da4
rosepose <rosepose@gmail.com>
2025-06-05 10:30:16 +0330

    feat: actor level envSync2 rule added for Hybrid Rebeca

---
+++ src/main/java/org/rebecalang/transparentactormodelchecker/hybridrebeca/actorlevelsosrules/HybridRebecaEnvSync2SOSRule.java
@@ -0,0 +1,26 @@
+package org.rebecalang.transparentactormodelchecker.hybridrebeca.actorlevelsosrules;
+
+import org.rebecalang.compiler.utils.Pair;
+import org.rebecalang.modelchecker.corerebeca.RebecaRuntimeInterpreterException;
+import org.rebecalang.transparentactormodelchecker.hybridrebeca.transitionsystem.action.TimeProgressAction;
+import org.rebecalang.transparentactormodelchecker.hybridrebeca.transitionsystem.state.HybridRebecaActorState;
+import org.rebecalang.transparentactormodelchecker.hybridrebeca.transitionsystem.transition.HybridRebecaDeterministicTransition;
+
+public class HybridRebecaEnvSync2SOSRule {
+
+    public HybridRebecaDeterministicTransition<HybridRebecaActorState> applyRule(HybridRebecaActorState source, int t) {
+        if(!source.messageQueueIsEmpty())
+            throw new RebecaRuntimeInterpreterException("Execution rule is disabled");
+
+        Pair<Integer, Integer> updatedNowTimeInterval = new Pair<>();
+        updatedNowTimeInterval.setFirst(source.getLowerBound(source.getNow()) + t);
+        updatedNowTimeInterval.setSecond(source.getUpperBound(source.getNow()) + t);
+        source.setNow(updatedNowTimeInterval);
+
+        HybridRebecaDeterministicTransition<HybridRebecaActorState> result =
+                new HybridRebecaDeterministicTransition<HybridRebecaActorState>();
+        result.setAction(new TimeProgressAction());
+        result.setDestination(source);
+        return result;
+    }
+}
